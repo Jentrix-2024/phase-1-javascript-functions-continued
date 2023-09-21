e9ee2d5827d9827075cdc52bbe518885e7e5588c
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-09-21 08:50:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(funActivity= "roller-skate"){
+    return(`This Saturday, I want to ${funActivity}!`);
+}
+const mondayWork = function (activity="go to the office"){
+return(`This Monday, I will ${activity}.`);
+}
+function wrapAdjective(parameter1='*'){
+return function( parameter="special"){
+ return (`You are ${parameter1}${parameter}${parameter1}!`)
+}
+}
